4bb2b3cbd587870fb4722dada40c17f70823d285
skiende74 <skiende74@gmail.com>
2023-08-11 16:11:38 +0900

    git check

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center sm:py-6">
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto sm:py-6 items-center ">
       <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
